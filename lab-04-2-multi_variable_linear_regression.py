54bfb0da0dee4f86d47e18db28b40d12704ad06d
Sung Kim <hunkim@gmail.com>
2017-02-23 18:14:01 +0800

    Let's use np when we need

--- lab-04-2-multi_variable_linear_regression.py
+++ lab-04-2-multi_variable_linear_regression.py
@@ -2,9 +2,9 @@
 import tensorflow as tf
 import numpy as np
 
-x_data = np.array([[1., 1.], [2., 2.], [3., 3.], [
-                  4., 4.], [5., 5.]], dtype=np.float32)
-y_data = np.array([1, 2, 3, 4, 5], dtype=np.float32).reshape(-1, 1)
+x_data = [[1., 1.], [2., 2.], [3., 3.],
+          [4., 4.], [5., 5.]]
+y_data = [[1], [2], [3], [4], [5]]
 
 W = tf.Variable(tf.random_uniform(
     shape=[2, 1], minval=-1.0, maxval=1.0, dtype=tf.float32))
@@ -21,7 +21,7 @@ cost = tf.reduce_mean(tf.square(hypothesis - y_data))
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(cost)
 
-# Initialize variable
+# Initialize variables
 init = tf.global_variables_initializer()
 
 # Launch graph
@@ -31,5 +31,5 @@ sess.run(init)
 for step in range(2001):
     sess.run(train)
     if step % 20 == 0:
-        print(step, sess.run(cost), sess.run(
-            hypothesis), sess.run(W), sess.run(b))
+        print(step, sess.run(cost),
+              sess.run(hypothesis), sess.run(W), sess.run(b))
