d2d0e33c2deb3c63f700240d86405ba8f25a20ae
Sung Kim <hunkim@gmail.com>
2017-02-28 07:39:38 +0800

    Added RNN 01 thanks to Jenny

---
+++ lab-12-1-hello-rnn.py
@@ -0,0 +1,49 @@
+# Lab 12 RNN
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)  # reproducibility
+
+dic = {0: 'h', 1: 'i', 2: 'e', 3: 'l', 4: 'o'}
+# Teach hello: hihell -> ihello
+x_data = [[0, 1, 0, 2, 3, 3]]   # hihell
+x_one_hot = [[[1, 0, 0, 0, 0],   # h 0
+              [0, 1, 0, 0, 0],   # i 1
+              [1, 0, 0, 0, 0],   # h 0
+              [0, 0, 1, 0, 0],   # e 2
+              [0, 0, 0, 1, 0],   # l 3
+              [0, 0, 0, 1, 0]]]  # l 3
+
+y_data = [[1, 0, 2, 3, 3, 4]]    # ihello
+
+input_dim = 5  # one-hot size
+hidden_size = 5  # output from the LSTM 4 to directly predict onehot
+batch_size = 1   # one sentence
+sequence_length = 6  # |ihello| == 6
+
+X = tf.placeholder(
+    tf.float32, [None, sequence_length, hidden_size])  # X one-hot
+Y = tf.placeholder(tf.int32, [None, sequence_length])  # Y label
+
+cell = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size, state_is_tuple=True)
+initial_state = cell.zero_state(batch_size, tf.float32)
+outputs, _states = tf.nn.dynamic_rnn(
+    cell, X, initial_state=initial_state, dtype=tf.float32)
+
+weights = tf.ones([batch_size, sequence_length])
+sequence_loss = tf.contrib.seq2seq.sequence_loss(
+    logits=outputs, targets=Y, weights=weights)
+loss = tf.reduce_mean(sequence_loss)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(loss)
+
+prediction = tf.argmax(outputs, axis=2)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(2000):
+        l, _ = sess.run([loss, train], feed_dict={X: x_one_hot, Y: y_data})
+        result = sess.run(prediction, feed_dict={X: x_one_hot})
+        print("loss:", l, "prediction: ", result, "true Y: ", y_data)
+
+        # print char using dic
+        result_str = [dic[c] for c in np.squeeze(result)]
+        print("Prediction str: ", ''.join(result_str))
