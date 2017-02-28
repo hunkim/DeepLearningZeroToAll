9c1433e6ff105046c760cc9682830d52d3dde8a4
Sung Kim <hunkim@gmail.com>
2017-02-28 20:15:59 +0800

    Added tf.gradient

---
+++ lab-03-4-minimizing_cost_tf_gradient.py
@@ -0,0 +1,52 @@
+# Lab 3 Minimizing Cost
+import tensorflow as tf
+tf.set_random_seed(777)  # for reproducibility
+
+# tf Graph Input
+X = [1, 2, 3]
+Y = [1, 2, 3]
+
+# Set wrong model weights
+W = tf.Variable(5.)
+
+# Linear model
+hypothesis = X * W
+
+# Manual gradient
+gradient = tf.reduce_mean((W * X - Y) * X)*2
+
+# Cost function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+# Minimize: Gradient Descent Magic
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+train = optimizer.minimize(cost)
+
+# Get gradients
+gvs = optimizer.compute_gradients(cost)
+# Apply gradients
+apply_gradients = optimizer.apply_gradients(gvs)
+
+# Launch the graph in a session.
+sess = tf.Session()
+# Initializes global variables in the graph.
+sess.run(tf.global_variables_initializer())
+
+for step in range(100):
+    print(step, sess.run([gradient, W, gvs]))
+    sess.run(apply_gradients)
+    # Same as sess.run(train)
+
+
+'''
+0 5.0
+1 4.62667
+2 4.28818
+3 3.98128
+4 3.70303
+...
+96 1.00033
+97 1.0003
+98 1.00027
+99 1.00025
+'''
