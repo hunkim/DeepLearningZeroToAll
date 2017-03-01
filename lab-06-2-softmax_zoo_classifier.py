ed00af328023dbf54a649d15265e46d4ac9ce9dc
Mo Kweon <kkweon@gmail.com>
2017-03-01 03:55:13 -0800

    refactor: softmax (#23)
    1) Fix softmax cross entropy
    2) Pretty Print Format
    3) Fix typo

--- lab-06-2-softmax_zoo_classifier.py
+++ lab-06-2-softmax_zoo_classifier.py
@@ -10,10 +10,10 @@ y_data = xy[:, [-1]]
 
 print(x_data.shape, y_data.shape)
 
-nb_classes = 7  # 1 ~ 7
+nb_classes = 7  # 0 ~ 6
 
-X = tf.placeholder("float", [None, 16])
-Y = tf.placeholder("int32", [None, 1])  # 1 ~ 7
+X = tf.placeholder(tf.float32, [None, 16])
+Y = tf.placeholder(tf.int32, [None, 1])  # 0 ~ 6
 Y_one_hot = tf.one_hot(Y, nb_classes)  # one hot
 Y_one_hot = tf.reshape(Y_one_hot, [-1, nb_classes])
 print(Y_one_hot)
@@ -23,41 +23,60 @@ b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
 
 # tf.nn.softmax computes softmax activations
 # softmax = exp(logits) / reduce_sum(exp(logits), dim)
-hypothesis = tf.nn.softmax(tf.matmul(X, W) + b)
+hypothesis = tf.matmul(X, W) + b
 
 # Cross entropy cost
-# cost = tf.reduce_mean(-tf.reduce_sum(Y *
-#        tf.log(hypothesis), axis=1))
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    labels=Y_one_hot, logits=hypothesis))
+# cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis=1))
+cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis,
+                                                 labels=Y_one_hot)
+cost = tf.reduce_mean(cost_i)
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 
-prediction = tf.argmax(hypothesis, 1)
 
+prediction = tf.argmax(hypothesis, 1)
+correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 # Launch graph
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    for step in range(20001):
+    for step in range(2000):
         sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
-        if step % 200 == 0:
-            print(step, sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+        if step % 100 == 0:
+            loss, acc = sess.run([cost, accuracy], feed_dict={X: x_data, Y: y_data})
+            print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(step, loss, acc))
 
     # Let's see if we can predict
     pred = sess.run(prediction, feed_dict={X: x_data})
-    for p, y in zip(pred, y_data):
-        print("prediction: ", p, " true Y: ", y)
+    # y_data: (N,1) = flatten => (N, ) matches pred.shape
+    for p, y in zip(pred, y_data.flatten()):
+        print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
 
 '''
-prediction:  2  true Y:  [ 3.]
-prediction:  4  true Y:  [ 4.]
-prediction:  1  true Y:  [ 1.]
-prediction:  1  true Y:  [ 1.]
-prediction:  2  true Y:  [ 2.]
-prediction:  1  true Y:  [ 1.]
-prediction:  6  true Y:  [ 6.]
-prediction:  1  true Y:  [ 1.]
-prediction:  0  true Y:  [ 7.]
-prediction:  2  true Y:  [ 2.]
+Step:     0 Loss: 5.106 Acc: 37.62%
+Step:   100 Loss: 0.800 Acc: 79.21%
+Step:   200 Loss: 0.486 Acc: 88.12%
+Step:   300 Loss: 0.349 Acc: 90.10%
+Step:   400 Loss: 0.272 Acc: 94.06%
+Step:   500 Loss: 0.222 Acc: 95.05%
+Step:   600 Loss: 0.187 Acc: 97.03%
+Step:   700 Loss: 0.161 Acc: 97.03%
+Step:   800 Loss: 0.140 Acc: 97.03%
+Step:   900 Loss: 0.124 Acc: 97.03%
+Step:  1000 Loss: 0.111 Acc: 97.03%
+Step:  1100 Loss: 0.101 Acc: 99.01%
+Step:  1200 Loss: 0.092 Acc: 100.00%
+Step:  1300 Loss: 0.084 Acc: 100.00%
+...
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 3 True Y: 3
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 0 True Y: 0
+[True] Prediction: 3 True Y: 3
+[True] Prediction: 3 True Y: 3
+[True] Prediction: 0 True Y: 0
 '''
