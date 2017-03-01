1e1188f97bf69faed88b461e35093421a23f8029
Sangwhan Moon <sangwhan@iki.fi>
2017-03-01 14:01:38 +0900

    Ensure data consistency over Keras and TF in 02-1.

--- lab-02-1-linear_regression.py
+++ lab-02-1-linear_regression.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 tf.set_random_seed(777)  # for reproducibility
 
 # X and Y data
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
 
 # Try to find values for W and b to compute y_data = x_data * W + b
 # We know that W should be 1 and b should be 0
