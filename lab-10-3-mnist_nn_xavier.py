5d94d9a220934329f0c2fa6fc33655147d2f187e
Jongmin <jijupax@gmail.com>
2017-03-03 02:04:20 +0900

    Update lab-10-3-mnist_nn_xavier.py

--- lab-10-3-mnist_nn_xavier.py
+++ lab-10-3-mnist_nn_xavier.py
@@ -76,14 +76,6 @@ print("Prediction: ", sess.run(
 # plt.imshow(mnist.test.images[r:r + 1].
 #           reshape(28, 28), cmap='Greys', interpolation='nearest')
 # plt.show()
-r = random.randint(0, mnist.test.num_examples - 1)
-print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
-print("Prediction: ", sess.run(
-    tf.argmax(hypothesis, 1), {X: mnist.test.images[r:r + 1]}))
-
-# plt.imshow(mnist.test.images[r:r + 1].
-#           reshape(28, 28), cmap='Greys', interpolation='nearest')
-# plt.show()
 
 '''
 Epoch: 0001 cost = 0.301498963
