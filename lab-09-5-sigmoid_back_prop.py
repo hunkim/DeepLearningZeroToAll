d43d347258672f94e1b78871e8efab96f33c5a07
Mo Kweon <kkweon@gmail.com>
2017-03-03 21:31:50 -0800

    refactor: reference, comment, loss,
    1. Description of file was added
      - add Prof. Sung KIM's slides and others
      - define loss function and network architecture
    
    2. Loss function change
      - In Prof. Sung KIM's slides, reduce_sum was used instead of
      reduce_mean. In this file, it's also modified to sync the slides
    
    3. Refactor X, y variable name
      - Scikit Learn's way to differentiate matrices and vectors
      - When we say 'X", the naming convention is to use uppercase because it's a multidimensional matrix
      - When we say "y", the naming convention is to use lowercase because it's a single vector

--- lab-09-5-sigmoid_back_prop.py
+++ lab-09-5-sigmoid_back_prop.py
@@ -1,78 +1,116 @@
-# http://blog.aloni.org/posts/backprop-with-tensorflow/
-# https://medium.com/@karpathy/yes-you-should-understand-backprop-e2f06eab496b#.b3rvzhx89
-# WIP
-#
+"""
+In this file, we will implement back propagations by hands
+
+We will use the Sigmoid Cross Entropy loss function.
+This is equivalent to tf.nn.sigmoid_softmax_with_logits(logits, labels)
+
+[References]
+
+1) Tensorflow Document (tf.nn.sigmoid_softmax_with_logits)
+    https://www.tensorflow.org/api_docs/python/tf/nn/sigmoid_cross_entropy_with_logits
+
+2) Neural Net Backprop in one slide! by Prof. Sung KIM
+    https://docs.google.com/presentation/d/1_ZmtfEjLmhbuM_PqbDYMXXLAqeWN0HwuhcSKnUQZ6MM/edit#slide=id.g1ec1d04b5a_1_83
+
+3) Back Propagation with Tensorflow by Dan Aloni
+    http://blog.aloni.org/posts/backprop-with-tensorflow/
+
+4) Yes you should understand backprop by Andrej Karpathy
+    https://medium.com/@karpathy/yes-you-should-understand-backprop-e2f06eab496b#.cockptkn7
+
+
+[Network Architecture]
+
+Input: x
+Layer1: x * W + b
+Output layer = σ(Layer1)
+
+Loss_i = - y * log(σ(Layer1)) - (1 - y) * log(1 - σ(Layer1))
+Loss = tf.reduce_sum(Loss_i)
+
+We want to compute that
+
+dLoss/dW = ???
+dLoss/db = ???
+
+please read Prof. Sung KIM's slides for each steps
+
+"""
 import tensorflow as tf
 import numpy as np
 tf.set_random_seed(777)  # for reproducibility
 
 # Predicting animal type based on various features
 xy = np.loadtxt('data-04-zoo.csv', delimiter=',', dtype=np.float32)
-x_data = xy[:, 0:-1]
-N = x_data.shape[0]
+X_data = xy[:, 0:-1]
+N = X_data.shape[0]
 y_data = xy[:, [-1]]
 
 # y_data has labels from 0 ~ 6
-# [0, 1, 2, 3, 4, 5, 6]
-print("Y has one of the following values")
+print("y has one of the following values")
 print(np.unique(y_data))
 
-# x_data.shape = (101, 16)
-# 101 samples, 16 features
-# y_data.shape = (101, 1)
-# 101 samples, 1 label (0 ~ 6)
-print("Shape of X data: ", x_data.shape)
-print("Shape of Y data: ", y_data.shape)
+# X_data.shape = (101, 16) => 101 samples, 16 features
+# y_data.shape = (101, 1)  => 101 samples, 1 label
+print("Shape of X data: ", X_data.shape)
+print("Shape of y data: ", y_data.shape)
 
 nb_classes = 7  # 0 ~ 6
 
 X = tf.placeholder(tf.float32, [None, 16])
-Y = tf.placeholder(tf.int32, [None, 1])  # 0 ~ 6
+y = tf.placeholder(tf.int32, [None, 1])  # 0 ~ 6
 
-Y_one_hot = tf.one_hot(Y, nb_classes)  # one hot
-Y_one_hot = tf.reshape(Y_one_hot, [-1, nb_classes])
-Y_one_hot = tf.cast(Y_one_hot, tf.float32)
+y_one_hot = tf.one_hot(y, nb_classes)  # one hot
+y_one_hot = tf.reshape(y_one_hot, [-1, nb_classes])
+y_one_hot = tf.cast(y_one_hot, tf.float32)
 
 W = tf.Variable(tf.random_normal([16, nb_classes]), name='weight')
 b = tf.Variable(tf.random_normal([nb_classes]), name='bias')
 
 
 def sigma(x):
-    #  sigmoid function
-    return tf.div(tf.constant(1.0),
-                  tf.add(tf.constant(1.0), tf.exp(-x)))
+    # sigmoid function
+    # σ(x) = 1 / (1 + exp(-x))
+    return 1. / (1. + tf.exp(-x))
 
 
 def sigma_prime(x):
     # derivative of the sigmoid function
-    return sigma(x) * (1 - sigma(x))
-
-# Forward prop
-layer1 = tf.add(tf.matmul(X, W), b)
-y_pred = sigma(layer1)
+    # σ'(x) = σ(x) * (1 - σ(x))
+    return sigma(x) * (1. - sigma(x))
 
 
-# diff
-assert y_pred.shape.as_list() == Y_one_hot.shape.as_list()
+# Forward propagtion
+layer_1 = tf.matmul(X, W) + b
+y_pred = sigma(layer_1)
 
-diff = (y_pred - Y_one_hot)
+# Loss Function (end of forwad propagation)
+loss_i = - y_one_hot * tf.log(y_pred) - (1. - y_one_hot) * tf.log(1. - y_pred)
+loss = tf.reduce_sum(loss_i)
+
+# Dimension Check
+assert y_pred.shape.as_list() == y_one_hot.shape.as_list()
+
 
 # Back prop (chain rule)
-d_layer1 = diff
-d_b = 1 * d_layer1
-d_w = tf.matmul(tf.transpose(X), d_layer1) / N
+# How to derive? please read Prof. Kim's slides
+d_loss = (y_pred - y_one_hot) / (y_pred * (1. - y_pred) + 1e-7)
+d_sigma = sigma_prime(layer_1)
+d_layer1 = d_loss * d_sigma
+
+d_b = tf.reduce_sum(d_layer1)
+d_W = tf.matmul(tf.transpose(X), d_layer1)
 
 # Updating network using gradients
-learning_rate = 0.5
+learning_rate = 0.01
 train_step = [
-    tf.assign(W, W - learning_rate * d_w),
-    tf.assign(b, b - learning_rate *
-              tf.reduce_mean(d_b)),
+    tf.assign(W, W - learning_rate * d_W),
+    tf.assign(b, b - learning_rate * d_b),
 ]
 
-# 7. Running and testing the training process
+# Prediction and Accuracy
 prediction = tf.argmax(y_pred, 1)
-acct_mat = tf.equal(tf.argmax(y_pred, 1), tf.argmax(Y_one_hot, 1))
+acct_mat = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_one_hot, 1))
 acct_res = tf.reduce_mean(tf.cast(acct_mat, tf.float32))
 
 # Launch graph
@@ -80,33 +118,34 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(500):
-        sess.run(train_step, feed_dict={X: x_data, Y: y_data})
+        sess.run(train_step, feed_dict={X: X_data, y: y_data})
 
         if step % 10 == 0:
-            # At 450 Step, you should see an accuracy of 100% 
-            acc = sess.run(acct_res, feed_dict={X: x_data, Y: y_data})
-            print("Step: {:5}\t Acc: {:.2%}".format(step, acc))
+            # Within 300 steps, you should see an accuracy of 100%
+            step_loss, acc = sess.run([loss, acct_res], feed_dict={X: X_data, y: y_data})
+            print("Step: {:5}\t Loss: {:10.5f}\t Acc: {:.2%}" .format(step, step_loss, acc))
 
     # Let's see if we can predict
-    pred = sess.run(prediction, feed_dict={X: x_data})
+    pred = sess.run(prediction, feed_dict={X: X_data})
     for p, y in zip(pred, y_data):
-        if p == y[0]:
-            msg = "Correct!\t Prediction: {:d}\t True Y: {:d}"
-        else:
-            msg = "Wrong!  \t Prediction: {:d}\t True Y: {:d}"
+        msg = "[{}]\t Prediction: {:d}\t True y: {:d}"
+        print(msg.format(p == int(y[0]), p, int(y[0])))
 
-        print(msg.format(p, int(y[0])))
+"""
+Output Example
 
-'''
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 3   True Y: 3
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 3   True Y: 3
-Correct!     Prediction: 3   True Y: 3
-Correct!     Prediction: 0   True Y: 0
-Correct!     Prediction: 0   True Y: 0
-'''
+Step:     0      Loss:  453.74799        Acc: 38.61%
+Step:    20      Loss:   95.05664        Acc: 88.12%
+Step:    40      Loss:   66.43570        Acc: 93.07%
+Step:    60      Loss:   53.09288        Acc: 94.06%
+...
+Step:   290      Loss:   18.72972        Acc: 100.00%
+Step:   300      Loss:   18.24953        Acc: 100.00%
+Step:   310      Loss:   17.79592        Acc: 100.00%
+...
+[True]   Prediction: 0   True y: 0
+[True]   Prediction: 0   True y: 0
+[True]   Prediction: 3   True y: 3
+[True]   Prediction: 0   True y: 0
+...
+"""
