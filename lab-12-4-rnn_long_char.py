fix softmax

lab-12-4-rnn_long_char.py
@@ -30,58 +30,47 @@ for i in range(0, len(sentence) - seq_length):
 batch_size = len(dataX) #170
 
 X = tf.placeholder(tf.int32, [None, seq_length])
-Y = tf.placeholder(tf.float32, [None, seq_length])
+Y = tf.placeholder(tf.int32, [None, seq_length])
 
 # One-hot encoding
 x_one_hot = tf.one_hot(X, num_classes)
-dropout = 0.5
 
 cell = tf.contrib.rnn.BasicLSTMCell(num_units=num_classes, state_is_tuple=True)
-# cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=dropout)
 cell = tf.contrib.rnn.MultiRNNCell([cell] * 2, state_is_tuple=True)
 initial_state = cell.zero_state(batch_size, tf.float32)
 
 outputs, _states = tf.nn.dynamic_rnn(cell, x_one_hot, dtype=tf.float32)
-last_output = outputs[:,-1]
+# outputs = tf.reshape(outputs, [batch_size * seq_length, num_classes])
+outputs = tf.reshape(outputs, [-1, num_classes])
 
-#softmax
-softmax= tf.get_variable("softmax", [num_classes, 1])
-softmax_output = tf.matmul(last_output,  softmax)
+w_softmax = tf.get_variable("w_softmax", [hidden_size, num_classes])
+b_softmax = tf.get_variable("b_softmax", [num_classes])
 
-#weights = tf.ones([batch_size, seq_length])
+softmax_output = tf.matmul(outputs, w_softmax) + b_softmax
+softmax_output = tf.reshape(softmax_output, [batch_size, seq_length, num_classes])
 
-#sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=softmax_output, targets=Y, weights=weights)
-sequence_loss = tf.reduce_sum(tf.square(softmax_output - Y))
+weights = tf.ones([batch_size, seq_length])
 
+sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=softmax_output, targets=Y, weights=weights)
 loss = tf.reduce_mean(sequence_loss)
 
-train = tf.train.GradientDescentOptimizer(learning_rate=0.0001).minimize(loss)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
 
-predict = tf.cast(tf.argmax(outputs, axis=-1), dtype=tf.float32)
-
-accuracy = tf.reduce_mean(tf.cast(tf.equal(predict, Y), dtype=tf.float32))
+predict = tf.argmax(softmax_output, axis=-1)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for x in range(2000):
-        l, _, a = sess.run([loss, train, accuracy], feed_dict={X: dataX, Y: dataY})
+        l, _ = sess.run([loss, train], feed_dict={X: dataX, Y: dataY})
         result = sess.run(predict, feed_dict={X: dataX})
-        x_index = dataX[0]
+     
+        print(x, "loss: ", l)
+
+    for i, prediction in enumerate(result):
+        x_index = dataX[i]
         x_str = [char_set[j] for j in x_index]
 
-        # index = result[0]
-        # result = [char_set[j] for j in index]
-        # print(''.join(x_str), ' -> ', ''.join(result))
+        index = prediction
+        result = [char_set[j] for j in index]
 
-
-
-        print(x, "loss: ", l, "accuracy: ", a)
-
-    # for i, prediction in enumerate(result):
-    #     x_index = dataX[i]
-    #     x_str = [char_set[j] for j in x_index]
-
-    #     index = prediction
-    #     result = [char_set[j] for j in index]
-
-    #     print(''.join(x_str), ' -> ', ''.join(result))
+        print(''.join(x_str), ' -> ', ''.join(result))