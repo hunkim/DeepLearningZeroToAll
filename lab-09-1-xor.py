bfe627492eb6886d1af946899491d6633651c495
LeeTaeMin <dnflxoals@gmail.com>
2017-03-10 15:45:05 +0900

    Update lab-09-1-xor.py
    DeepLearningZeroToAll 다른 코드들과의 통일성과 Readability, Understandability를 위해 shape를 추가하면 좋을 것 같습니다.

--- lab-09-1-xor.py
+++ lab-09-1-xor.py
@@ -7,8 +7,8 @@ tf.set_random_seed(777)  # for reproducibility
 x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
 y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+X = tf.placeholder(tf.float32,[None,2])
+Y = tf.placeholder(tf.float32,[None,1])
 
 W = tf.Variable(tf.random_normal([2, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
