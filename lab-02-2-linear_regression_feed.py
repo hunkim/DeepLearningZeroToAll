f293c86d9e8264fef1c615eb8f68a32c69347a55
Sung Kim <hunkim@gmail.com>
2017-03-13 07:58:03 +0900

    more interesting ecamples

--- lab-02-2-linear_regression_feed.py
+++ lab-02-2-linear_regression_feed.py
@@ -2,9 +2,6 @@
 import tensorflow as tf
 tf.set_random_seed(777)  # for reproducibility
 
-x_train = [1, 2, 3]
-y_train = [1, 2, 3]
-
 # Try to find values for W and b to compute y_data = W * x_data + b
 # We know that W should be 1 and b should be 0
 # But let's use TensorFlow to figure it out
@@ -14,8 +11,8 @@ b = tf.Variable(tf.random_normal([1]), name='bias')
 # Now we can use X and Y in place of x_data and y_data
 # # placeholders for a tensor that will be always fed using feed_dict
 # See http://stackoverflow.com/questions/36693740/
-X = tf.placeholder(tf.float32)
-Y = tf.placeholder(tf.float32)
+X = tf.placeholder(tf.float32, shape=[None])
+Y = tf.placeholder(tf.float32, shape=[None])
 
 # Our hypothesis XW+b
 hypothesis = X * W + b
@@ -34,10 +31,11 @@ sess.run(tf.global_variables_initializer())
 
 # Fit the line
 for step in range(2001):
-    sess.run(train, feed_dict={X: x_train, Y: y_train})
+    cost_val, W_val, b_val, _ = \
+        sess.run([cost, W, b, train],
+                 feed_dict={X: [1, 2, 3], Y: [1, 2, 3]})
     if step % 20 == 0:
-        print(step, sess.run(cost, feed_dict={
-              X: x_train, Y: y_train}), sess.run(W), sess.run(b))
+        print(step, cost_val, W_val, b_val)
 
 # Learns best fit W:[ 1.],  b:[ 0]
 '''
@@ -47,10 +45,36 @@ for step in range(2001):
 '''
 
 # Testing our model
-print(sess.run(hypothesis, feed_dict={X: 5}))
-print(sess.run(hypothesis, feed_dict={X: 2.5}))
+print(sess.run(hypothesis, feed_dict={X: [5]}))
+print(sess.run(hypothesis, feed_dict={X: [2.5]}))
+print(sess.run(hypothesis, feed_dict={X: [1.5, 3.5]}))
 
 '''
 [ 5.0110054]
 [ 2.50091505]
+[ 1.49687922  3.50495124]
+'''
+
+
+# Fit the line with new training data
+for step in range(2001):
+    cost_val, W_val, b_val, _ = \
+        sess.run([cost, W, b, train],
+                 feed_dict={X: [1, 2, 3, 4, 5],
+                            Y: [2.1, 3.1, 4.1, 5.1, 6.1]})
+    if step % 20 == 0:
+        print(step, cost_val, W_val, b_val)
+
+# Testing our model
+print(sess.run(hypothesis, feed_dict={X: [5]}))
+print(sess.run(hypothesis, feed_dict={X: [2.5]}))
+print(sess.run(hypothesis, feed_dict={X: [1.5, 3.5]}))
+
+'''
+1960 3.32396e-07 [ 1.00037301] [ 1.09865296]
+1980 2.90429e-07 [ 1.00034881] [ 1.09874094]
+2000 2.5373e-07 [ 1.00032604] [ 1.09882331]
+[ 6.10045338]
+[ 3.59963846]
+[ 2.59931231  4.59996414]
 '''
