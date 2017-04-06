7537a27c87e0877d630f66dd341fcfd7481d956a
Hyungjoo Andrew Cho <phelahab@gmail.com>
2017-04-07 08:08:32 +0900

    Create lab-05-2-logistic_regression_diabetes_with_vectorized_form.py
    add script of vectorized form for logistic loss

---
+++ lab-05-2-logistic_regression_diabetes_with_vectorized_form.py
@@ -0,0 +1,45 @@
+# Lab 5 Logistic Regression Classifier
+import tensorflow as tf
+import numpy as np
+tf.set_random_seed(777)  # for reproducibility
+
+xy = np.loadtxt('data-03-diabetes.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, 0:-1]
+y_data = xy[:, [-1]]
+
+print(x_data.shape, y_data.shape)
+
+# placeholders for a tensor that will be always fed.
+X = tf.placeholder(tf.float32, shape=[None, 8])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([8, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+# Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
+hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+
+# cost/loss function with vectorized form
+cost = -(tf.matmul(tf.log(hypothesis), Y, transpose_a=True) + tf.matmul(tf.log(1-hypothesis), 1-Y, transpose_a=True))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+# Launch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(10001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+        if step % 200 == 0:
+            print(step, cost_val)
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy],
+                       feed_dict={X: x_data, Y: y_data})
+    print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a)
