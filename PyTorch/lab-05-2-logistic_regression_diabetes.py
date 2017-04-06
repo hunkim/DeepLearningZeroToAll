7baa78e346fd540fbd785820dd47bbd78ca96ee2
togheppi <licslegend@gmail.com>
2017-04-06 23:03:36 +0900

    Update lab-05-2-logistic_regression_diabetes.py

--- PyTorch/lab-05-2-logistic_regression_diabetes.py
+++ PyTorch/lab-05-2-logistic_regression_diabetes.py
@@ -1,13 +1,9 @@
-
-# coding: utf-8
-
-# In[1]:
-
+# Lab 5 Logistic Regression Classifier
 import torch
 from torch.autograd import Variable 
 import numpy as np
 
-torch.manual_seed(777)
+torch.manual_seed(777)  # for reproducibility
 
 xy = np.loadtxt('data-03-diabetes.csv', delimiter=',', dtype=np.float32)
 x_data = xy[:, 0:-1]
@@ -19,21 +15,17 @@ print(x_data.shape, y_data.shape)
 X = Variable(torch.from_numpy(x_data))
 Y = Variable(torch.from_numpy(y_data))
 
-# Our model
+# Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
 linear = torch.nn.Linear(8,1,bias=True)
 sigmoid = torch.nn.Sigmoid()
-
 model = torch.nn.Sequential(linear,sigmoid)
-# model:add(linear)
-# model:add(sigmoid)
 
-# criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(),lr=0.01)
 
 for step in range(10001):
     optimizer.zero_grad()
     hypothesis = model(X)
-#     cost = criterion(hypothesis,Y)
+    # cost/loss function
     cost = -(Y*torch.log(hypothesis) + (1-Y)*torch.log(1-hypothesis)).mean()
     cost.backward()
     optimizer.step()
@@ -41,7 +33,7 @@ for step in range(10001):
     if step % 200 == 0:
         print(step, cost.data.numpy())
         
-    
+# Accuracy computation
 predicted = (model(X).data > 0.5).float()
 accuracy = (predicted == Y.data).float().mean()
 print("\nHypothesis: ", hypothesis.data.numpy(), "\nCorrect (Y): ", predicted.numpy(), "\nAccuracy: ", accuracy)
