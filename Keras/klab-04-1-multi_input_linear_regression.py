2f8ce1ef81f0a55a5f1cc873d0e2d31ae3ec23d1
skyer9 <skyer9@gmail.com>
2017-04-09 15:06:33 +0900

    change list indentation style.

--- Keras/klab-04-1-multi_input_linear_regression.py
+++ Keras/klab-04-1-multi_input_linear_regression.py
@@ -2,9 +2,16 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 
-x_data = [[73., 80., 75.], [93., 88., 93.], [
-    89., 91., 90.], [96., 98., 100.], [73., 66., 70.]]
-y_data = [[152.], [185.], [180.], [196.], [142.]]
+x_data = [[73., 80., 75.],
+          [93., 88., 93.],
+          [89., 91., 90.],
+          [96., 98., 100.],
+          [73., 66., 70.]]
+y_data = [[152.],
+          [185.],
+          [180.],
+          [196.],
+          [142.]]
 
 model = Sequential()
 model.add(Dense(input_dim=3, units=1))
