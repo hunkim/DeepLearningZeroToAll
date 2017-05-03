9875acfd8c83fd3372f3449e01602c6f099aa395
skyer9 <skyer9@gmail.com>
2017-05-04 06:41:49 +0900

    modify learning rate for linear regression. (#143)

--- Keras/klab-02-1-linear_regression.py
+++ Keras/klab-02-1-linear_regression.py
@@ -1,6 +1,8 @@
-from keras.models import Sequential
-from keras.layers import Dense
 import numpy as np
+from keras import optimizers
+from keras.layers import Dense
+from keras.models import Sequential
+
 
 x_train = [1, 2, 3, 4]
 y_train = [0, -1, -2, -3]
@@ -8,12 +10,13 @@ y_train = [0, -1, -2, -3]
 model = Sequential()
 model.add(Dense(1, input_dim=1))
 
-model.compile(loss='mse', optimizer='sgd')
+sgd = optimizers.SGD(lr=0.1)
+model.compile(loss='mse', optimizer=sgd)
 
 # prints summary of the model to the terminal
 model.summary()
 
-model.fit(x_train, y_train, epochs=1000)
+model.fit(x_train, y_train, epochs=200)
 
-y_predict = model.predict(np.array([4]))
+y_predict = model.predict(np.array([5]))
 print(y_predict)
