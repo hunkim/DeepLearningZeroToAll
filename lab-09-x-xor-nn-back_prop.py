4bfcd1eeb96fc75991f8f0374cc2540b3fb437a8
Sung Kim <hunkim@gmail.com>
2017-06-12 15:01:11 +0900

    Fixed XOR back prop. TODO: Fix other back props

--- lab-09-x-xor-nn-back_prop.py
+++ lab-09-x-xor-nn-back_prop.py
@@ -13,6 +13,7 @@ y_data = [[0],
           [1],
           [1],
           [0]]
+
 x_data = np.array(x_data, dtype=np.float32)
 y_data = np.array(y_data, dtype=np.float32)
 
@@ -21,46 +22,60 @@ Y = tf.placeholder(tf.float32, [None, 1])
 
 W1 = tf.Variable(tf.random_normal([2, 2]), name='weight1')
 b1 = tf.Variable(tf.random_normal([2]), name='bias1')
-layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+l1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 
 W2 = tf.Variable(tf.random_normal([2, 1]), name='weight2')
 b2 = tf.Variable(tf.random_normal([1]), name='bias2')
-hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+Y_pred = tf.sigmoid(tf.matmul(l1, W2) + b2)
 
 # cost/loss function
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
-                       tf.log(1 - hypothesis))
-def sigma(x):
-    # sigmoid function
-    # σ(x) = 1 / (1 + exp(-x))
-    return 1. / (1. + tf.exp(-x))
+cost = -tf.reduce_mean(Y * tf.log(Y_pred) + (1 - Y) *
+                       tf.log(1 - Y_pred))
 
+# Network
+#          p1     a1           l1     p2     a2           l2 (y_pred)
+# X -> (*) -> (+) -> (sigmoid) -> (*) -> (+) -> (sigmoid) -> (loss)
+#       ^      ^                   ^      ^
+#       |      |                   |      |
+#       W1     b1                  W2     b2
 
-def sigma_prime(x):
-    # derivative of the sigmoid function
-    # σ'(x) = σ(x) * (1 - σ(x))
-    return sigma(x) * (1. - sigma(x))
+# Loss derivative
+d_Y_pred = (Y_pred - Y) / (Y_pred * (1.0 - Y_pred) + 1e-7)
 
-diff = hypothesis - Y
+# Layer 2
+d_sigma2 = Y_pred * (1 - Y_pred)
+d_a2 = d_Y_pred * d_sigma2
+d_p2 = d_a2
+d_b2 = d_a2
+d_W2 = tf.matmul(tf.transpose(l1), d_p2)
 
-d_l2 = tf.multiply(diff, sigma_prime(tf.matmul(layer1, W2) + b2))
-d_b2 = d_l2
-d_W2 = tf.matmul(tf.transpose(layer1), d_l2)
+# Mean
+d_b2_mean = tf.reduce_mean(d_b2, axis=[0])
+d_W2_mean = d_W2 / tf.cast(tf.shape(l1)[0], dtype=tf.float32)
 
-d_l1 = tf.multiply(tf.matmul(d_l2, tf.transpose(W2)), sigma_prime(tf.matmul(X, W1) + b1))
-d_b1 = d_l1
-d_W1 = tf.matmul(tf.transpose(X), d_l1)
+# Layer 1
+d_l1 = tf.matmul(d_p2, tf.transpose(W2))
+d_sigma1 = l1 * (1 - l1)
+d_a1 = d_l1 * d_sigma1
+d_b1 = d_a1
+d_p1 = d_a1
+d_W1 = tf.matmul(tf.transpose(X), d_a1)
 
+# Mean
+d_W1_mean = d_W1 / tf.cast(tf.shape(X)[0], dtype=tf.float32)
+d_b1_mean = tf.reduce_mean(d_b1, axis=[0])
+
+# Weight update
 step = [
-  tf.assign(W2, W2 - learning_rate * d_W2),
-  tf.assign(b2, b2 - learning_rate * tf.reduce_mean(d_b2, axis=[0])),
-  tf.assign(W1, W1 - learning_rate * d_W1),
-  tf.assign(b1, b1 - learning_rate * tf.reduce_mean(d_b1, axis=[0]))
+  tf.assign(W2, W2 - learning_rate * d_W2_mean),
+  tf.assign(b2, b2 - learning_rate * d_b2_mean),
+  tf.assign(W1, W1 - learning_rate * d_W1_mean),
+  tf.assign(b1, b1 - learning_rate * d_b1_mean)
 ]
 
 # Accuracy computation
 # True if hypothesis > 0.5 else False
-predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+predicted = tf.cast(Y_pred > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 
 # Launch graph
@@ -68,14 +83,17 @@ with tf.Session() as sess:
     # Initialize TensorFlow variables
     sess.run(tf.global_variables_initializer())
 
+    print("shape", sess.run(tf.shape(X)[0], feed_dict={X: x_data}))
+
+
     for i in range(10001):
         sess.run([step, cost], feed_dict={X: x_data, Y: y_data})
         if i % 1000 == 0:
-            print(i, sess.run(cost, feed_dict={
+            print(i, sess.run([cost, d_W1], feed_dict={
                   X: x_data, Y: y_data}), sess.run([W1, W2]))
 
     # Accuracy report
-    h, c, a = sess.run([hypothesis, predicted, accuracy],
+    h, c, a = sess.run([Y_pred, predicted, accuracy],
                        feed_dict={X: x_data, Y: y_data})
     print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
 
