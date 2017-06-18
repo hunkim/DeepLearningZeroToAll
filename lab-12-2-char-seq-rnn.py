0ba3c7967b3f1d4cb02a83de6db68b6f45161f50
Soonmok Kwon <ep1804@users.noreply.github.com>
2017-06-19 00:33:29 +0900

    Update lab-12-2-char-seq-rnn.py
    X_for_fc used

--- lab-12-2-char-seq-rnn.py
+++ lab-12-2-char-seq-rnn.py
@@ -31,7 +31,7 @@ outputs, _states = tf.nn.dynamic_rnn(
 
 # FC layer
 X_for_fc = tf.reshape(outputs, [-1, hidden_size])
-outputs = tf.contrib.layers.fully_connected(outputs, num_classes, activation_fn=None)
+outputs = tf.contrib.layers.fully_connected(X_for_fc, num_classes, activation_fn=None)
 
 # reshape out for sequence_loss
 outputs = tf.reshape(outputs, [batch_size, sequence_length, num_classes])
