905b3d070217e117e2c39f5cb7b4e7ae17617ec8
HanbumKo <37624747+HanbumKo@users.noreply.github.com>
2018-05-08 21:06:15 +0900

    move calculating total_batch to outside

--- lab-10-5-mnist_nn_dropout.py
+++ lab-10-5-mnist_nn_dropout.py
@@ -15,6 +15,7 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 learning_rate = 0.001
 training_epochs = 15
 batch_size = 100
+total_batch = int(mnist.train.num_examples / batch_size)
 
 # input place holders
 X = tf.placeholder(tf.float32, [None, 784])
@@ -66,7 +67,6 @@ sess.run(tf.global_variables_initializer())
 # train my model
 for epoch in range(training_epochs):
     avg_cost = 0
-    total_batch = int(mnist.train.num_examples / batch_size)
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
