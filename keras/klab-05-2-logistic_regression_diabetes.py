b0b80221e0e736ed663b414c845f6ab245741c3e
Jongpil Kim <sragin@naver.com>
2018-06-15 16:11:49 +0900

    Update klab-05-2-logistic_regression_diabetes.py
    data-03-diabetes.csv 파일을 읽어와서 학습 후 결과를 출력하도록 수정

--- keras/klab-05-2-logistic_regression_diabetes.py
+++ keras/klab-05-2-logistic_regression_diabetes.py
@@ -3,21 +3,12 @@ from keras.layers import Dense
 from keras.optimizers import SGD
 import numpy as np
 
-x_data = [[1, 2],
-          [2, 3],
-          [3, 1],
-          [4, 3],
-          [5, 3],
-          [6, 2]]
-y_data = [[0],
-          [0],
-          [0],
-          [1],
-          [1],
-          [1]]
+xy = np.loadtxt('data-03-diabetes.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:,:-1]
+y_data = xy[:,-1]
 
 model = Sequential()
-model.add(Dense(1, input_dim=2, activation='sigmoid'))
+model.add(Dense(1, input_dim=8, activation='sigmoid'))
 
 sgd = SGD(lr=0.1)
 model.compile(loss='binary_crossentropy', optimizer=sgd)
@@ -25,5 +16,7 @@ model.compile(loss='binary_crossentropy', optimizer=sgd)
 model.summary()
 model.fit(x_data, y_data, epochs=2000)
 
-print("2,1", model.predict_classes(np.array([[2, 1]])))
-print("6,5", model.predict_classes(np.array([[6, 5]])))
+h = model.predict(x_train)
+c = np.float32(h > 0.5)
+a = np.mean(np.equal(c, y_train))
+print("\nHypothesis: \n", h, "\nCorrect (Y): \n", c, "\nAccuracy: ", a)
