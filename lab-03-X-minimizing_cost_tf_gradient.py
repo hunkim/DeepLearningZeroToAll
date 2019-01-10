f520916413fe349b5497c1c3690cd31236be3d37
qoocrab <qoocrab@gmail.com>
2019-01-10 15:22:26 +0900

    Update lab-03-X-minimizing_cost_tf_gradient.py
    1. The value of W changes because of 'apply_gradients', not because of 'gradient'. Therefore, it is not necessary to make W output twice.
    2. Modified output comment.
    3. 'train = optimizer.minimize (cost)' is an unnecessary sentence.

--- lab-03-X-minimizing_cost_tf_gradient.py
+++ lab-03-X-minimizing_cost_tf_gradient.py
@@ -21,7 +21,6 @@ cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize: Gradient Descent Magic
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
-train = optimizer.minimize(cost)
 
 # Get gradients
 gvs = optimizer.compute_gradients(cost, [W])
@@ -36,21 +35,18 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for step in range(100):
-    print(step, sess.run([gradient, W, gvs]))
-    sess.run(apply_gradients)
-    # Same as sess.run(train)
-
+    gradient_val, gvs_val, _ = sess.run([gradient, gvs, apply_gradients]) # Same as sess.run(train)
+    print(step, gradient_val, gvs_val)
 
 '''
-# Apply gradients
-0 [37.333332, 5.0, [(37.333336, 5.0)]]
-1 [33.848888, 4.6266665, [(33.848888, 4.6266665)]]
-2 [30.689657, 4.2881775, [(30.689657, 4.2881775)]]
-3 [27.825287, 3.9812808, [(27.825287, 3.9812808)]]
-4 [25.228262, 3.703028, [(25.228264, 3.703028)]]
+0 37.333332 [(37.333336, 5.0)]
+1 33.84889 [(33.84889, 4.6266665)]
+2 30.689657 [(30.689657, 4.2881775)]
+3 27.825289 [(27.825289, 3.981281)]
+4 25.228264 [(25.228264, 3.7030282)]
 ...
-96 [0.0030694802, 1.0003289, [(0.0030694804, 1.0003289)]]
-97 [0.0027837753, 1.0002983, [(0.0027837753, 1.0002983)]]
-98 [0.0025234222, 1.0002704, [(0.0025234222, 1.0002704)]]
-99 [0.0022875469, 1.0002451, [(0.0022875469, 1.0002451)]]
+96 0.0030694802 [(0.0030694804, 1.0003289)]
+97 0.0027837753 [(0.0027837753, 1.0002983)]
+98 0.0025234222 [(0.0025234222, 1.0002704)]
+99 0.0022875469 [(0.0022875469, 1.0002451)]
 '''
