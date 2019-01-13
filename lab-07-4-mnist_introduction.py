db9125b77ef7eb679d09db31282c6ee57dd713b2
qoocrab <qoocrab@gmail.com>
2019-01-13 14:56:49 +0900

    Update lab-07-4-mnist_introduction.py
    1. Change arg_max to argmax.
    2. Change the name of the variable 'total_batch' to 'num_iterations' to understand the concept of 'iterations' mentioned in the lecture.

--- lab-07-4-mnist_introduction.py
+++ lab-07-4-mnist_introduction.py
@@ -1,10 +1,12 @@
 # Lab 7 Learning rate and Evaluation
 import tensorflow as tf
-import random
 import matplotlib.pyplot as plt
+import random
+
 tf.set_random_seed(777)  # for reproducibility
 
 from tensorflow.examples.tutorials.mnist import input_data
+
 # Check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -23,69 +25,74 @@ b = tf.Variable(tf.random_normal([nb_classes]))
 hypothesis = tf.nn.softmax(tf.matmul(X, W) + b)
 
 cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(hypothesis), axis=1))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 
 # Test model
-is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
+is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 # Calculate accuracy
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 # parameters
-training_epochs = 15
+num_epochs = 15
 batch_size = 100
+num_iterations = int(mnist.train.num_examples / batch_size)
 
 with tf.Session() as sess:
     # Initialize TensorFlow variables
     sess.run(tf.global_variables_initializer())
     # Training cycle
-    for epoch in range(training_epochs):
+    for epoch in range(num_epochs):
         avg_cost = 0
-        total_batch = int(mnist.train.num_examples / batch_size)
 
-        for i in range(total_batch):
+        for i in range(num_iterations):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            c, _ = sess.run([cost, optimizer], feed_dict={
-                            X: batch_xs, Y: batch_ys})
-            avg_cost += c / total_batch
+            _, cost_val = sess.run([train, cost], feed_dict={X: batch_xs, Y: batch_ys})
+            avg_cost += cost_val / num_iterations
 
-        print('Epoch:', '%04d' % (epoch + 1),
-              'cost =', '{:.9f}'.format(avg_cost))
+        print("Epoch: {:04d}, Cost: {:.9f}".format(epoch + 1, avg_cost))
 
     print("Learning finished")
 
     # Test the model using test sets
-    print("Accuracy: ", accuracy.eval(session=sess, feed_dict={
-          X: mnist.test.images, Y: mnist.test.labels}))
+    print(
+        "Accuracy: ",
+        accuracy.eval(
+            session=sess, feed_dict={X: mnist.test.images, Y: mnist.test.labels}
+        ),
+    )
 
     # Get one and predict
     r = random.randint(0, mnist.test.num_examples - 1)
-    print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
-    print("Prediction: ", sess.run(
-        tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+    print("Label: ", sess.run(tf.argmax(mnist.test.labels[r : r + 1], 1)))
+    print(
+        "Prediction: ",
+        sess.run(tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r : r + 1]}),
+    )
 
     plt.imshow(
-        mnist.test.images[r:r + 1].reshape(28, 28),
-        cmap='Greys',
-        interpolation='nearest')
+        mnist.test.images[r : r + 1].reshape(28, 28),
+        cmap="Greys",
+        interpolation="nearest",
+    )
     plt.show()
 
 
 '''
-Epoch: 0001 cost = 2.868104637
-Epoch: 0002 cost = 1.134684615
-Epoch: 0003 cost = 0.908220728
-Epoch: 0004 cost = 0.794199896
-Epoch: 0005 cost = 0.721815854
-Epoch: 0006 cost = 0.670184430
-Epoch: 0007 cost = 0.630576546
-Epoch: 0008 cost = 0.598888191
-Epoch: 0009 cost = 0.573027079
-Epoch: 0010 cost = 0.550497213
-Epoch: 0011 cost = 0.532001859
-Epoch: 0012 cost = 0.515517795
-Epoch: 0013 cost = 0.501175288
-Epoch: 0014 cost = 0.488425370
-Epoch: 0015 cost = 0.476968593
+Epoch: 0001, Cost: 2.826302672
+Epoch: 0002, Cost: 1.061668952
+Epoch: 0003, Cost: 0.838061315
+Epoch: 0004, Cost: 0.733232745
+Epoch: 0005, Cost: 0.669279885
+Epoch: 0006, Cost: 0.624611836
+Epoch: 0007, Cost: 0.591160344
+Epoch: 0008, Cost: 0.563868987
+Epoch: 0009, Cost: 0.541745171
+Epoch: 0010, Cost: 0.522673578
+Epoch: 0011, Cost: 0.506782325
+Epoch: 0012, Cost: 0.492447643
+Epoch: 0013, Cost: 0.479955837
+Epoch: 0014, Cost: 0.468893674
+Epoch: 0015, Cost: 0.458703488
 Learning finished
-Accuracy:  0.888
+Accuracy:  0.8951
 '''
