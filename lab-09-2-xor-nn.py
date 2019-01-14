c644f047ca498c07dc0a28c9851ec1539d9b5865
qoocrab <qoocrab@gmail.com>
2019-01-14 15:36:27 +0900

    Update lab-09-2-xor-nn.py (#237)
    * Update lab-09-2-xor-nn.py
    
    1. Removed unnecessary codes.
    
    * Update lab-09-2-xor-nn.py
    
    Add numpy again.
    
    * Update lab-09-2-xor-nn.py
    
    Add numpy again.
    
    * Update lab-09-2-xor-nn.py
    
    Commit suggstion.
    
    Co-Authored-By: qoocrab <qoocrab@gmail.com>
    
    * Update lab-09-2-xor-nn.py
    
    Commit Suggestion.
    
    Co-Authored-By: qoocrab <qoocrab@gmail.com>
    
    * Update lab-09-2-xor-nn.py
    
    Commit Suggestion.
    
    Co-Authored-By: qoocrab <qoocrab@gmail.com>
    
    * Update lab-09-2-xor-nn.py
    
    Add f-string output.
    
    * Update lab-09-2-xor-nn.py

--- lab-09-2-xor-nn.py
+++ lab-09-2-xor-nn.py
@@ -3,18 +3,9 @@ import tensorflow as tf
 import numpy as np
 
 tf.set_random_seed(777)  # for reproducibility
-learning_rate = 0.1
 
-x_data = [[0, 0],
-          [0, 1],
-          [1, 0],
-          [1, 1]]
-y_data = [[0],
-          [1],
-          [1],
-          [0]]
-x_data = np.array(x_data, dtype=np.float32)
-y_data = np.array(y_data, dtype=np.float32)
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
 X = tf.placeholder(tf.float32, [None, 2])
 Y = tf.placeholder(tf.float32, [None, 1])
@@ -28,10 +19,8 @@ b2 = tf.Variable(tf.random_normal([1]), name='bias2')
 hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
 # cost/loss function
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
-                       tf.log(1 - hypothesis))
-
-train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 
 # Accuracy computation
 # True if hypothesis>0.5 else False
@@ -44,25 +33,29 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        _, cost_val = sess.run([train, cost], feed_dict={X: x_data, Y: y_data})
         if step % 100 == 0:
-            print(step, sess.run(cost, feed_dict={
-                  X: x_data, Y: y_data}), sess.run([W1, W2]))
+            print(step, cost_val)
 
     # Accuracy report
-    h, c, a = sess.run([hypothesis, predicted, accuracy],
-                       feed_dict={X: x_data, Y: y_data})
-    print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
+    h, p, a = sess.run(
+        [hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data}
+    )
+    
+    print(f"\nHypothesis:\n{h} \nPredicted:\n{p} \nAccuracy:\n{a}")
 
 
 '''
-Hypothesis:  [[ 0.01338218]
- [ 0.98166394]
- [ 0.98809403]
- [ 0.01135799]]
-Correct:  [[ 0.]
- [ 1.]
- [ 1.]
- [ 0.]]
-Accuracy:  1.0
+Hypothesis:
+[[0.01338216]
+ [0.98166394]
+ [0.98809403]
+ [0.01135799]] 
+Predicted:
+[[0.]
+ [1.]
+ [1.]
+ [0.]] 
+Accuracy:
+1.0
 '''
