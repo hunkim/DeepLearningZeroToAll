d1461d423863b36d3e72f68e30b78f6c8b72298b
qoocrab <qoocrab@gmail.com>
2019-01-14 15:31:31 +0900

    Update lab-09-1-xor.py (#236)
    * Update lab-09-1-xor.py
    
    Removed unnecessary codes.
    
    * Update lab-09-1-xor.py
    
    Add numpy again.
    
    * Update lab-09-1-xor.py
    
    Commit suggestion.

--- lab-09-1-xor.py
+++ lab-09-1-xor.py
@@ -3,33 +3,22 @@ import tensorflow as tf
 import numpy as np
 
 tf.set_random_seed(777)  # for reproducibility
-learning_rate = 0.1
 
-x_data = [[0, 0],
-          [0, 1],
-          [1, 0],
-          [1, 1]]
-y_data = [[0],
-          [1],
-          [1],
-          [0]]
-x_data = np.array(x_data, dtype=np.float32)
-y_data = np.array(y_data, dtype=np.float32)
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y_data = np.array([[0], [1], [1], [0]], dtype=np.float32)
 
 X = tf.placeholder(tf.float32, [None, 2])
 Y = tf.placeholder(tf.float32, [None, 1])
 
-W = tf.Variable(tf.random_normal([2, 1]), name='weight')
-b = tf.Variable(tf.random_normal([1]), name='bias')
+W = tf.Variable(tf.random_normal([2, 1]), name="weight")
+b = tf.Variable(tf.random_normal([1]), name="bias")
 
 # Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
 hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
 
 # cost/loss function
-cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
-                       tf.log(1 - hypothesis))
-
-train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
+train = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
 
 # Accuracy computation
 # True if hypothesis>0.5 else False
@@ -42,14 +31,16 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        sess.run(train, feed_dict={X: x_data, Y: y_data})
+        _, cost_val, w_val = sess.run(
+                  [train, cost, W], feed_dict={X: x_data, Y: y_data}
+        )
         if step % 100 == 0:
-            print(step, sess.run(cost, feed_dict={
-                  X: x_data, Y: y_data}), sess.run(W))
+            print(step, cost_val, w_val)
 
     # Accuracy report
-    h, c, a = sess.run([hypothesis, predicted, accuracy],
-                       feed_dict={X: x_data, Y: y_data})
+    h, c, a = sess.run(
+              [hypothesis, predicted, accuracy], feed_dict={X: x_data, Y: y_data}
+    )
     print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
 
 '''
