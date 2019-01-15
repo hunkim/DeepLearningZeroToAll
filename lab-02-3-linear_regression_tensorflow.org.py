edf393eaed88f55e7bb25e857a98ab9a98b7ee16
qoocrab <qoocrab@gmail.com>
2019-01-15 13:06:41 +0900

    Update Lab-02, Lab-03  (#240)
    * Update lab-03-X-minimizing_cost_tf_gradient.py
    
    1. The value of W changes because of 'apply_gradients', not because of 'gradient'. Therefore, it is not necessary to make W output twice.
    2. Modified output comment.
    3. 'train = optimizer.minimize (cost)' is an unnecessary sentence.
    
    * Modify the code for readability. (Black formatting)

--- lab-02-3-linear_regression_tensorflow.org.py
+++ lab-02-3-linear_regression_tensorflow.org.py
@@ -1,34 +1,37 @@
 # From https://www.tensorflow.org/get_started/get_started
 import tensorflow as tf
 
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
 # Model parameters
-W = tf.Variable([.3], tf.float32)
-b = tf.Variable([-.3], tf.float32)
+W = tf.Variable([0.3], tf.float32)
+b = tf.Variable([-0.3], tf.float32)
 
 # Model input and output
 x = tf.placeholder(tf.float32)
 y = tf.placeholder(tf.float32)
 
-linear_model = x * W + b
+hypothesis = x * W + b
 
 # cost/loss function
-loss = tf.reduce_sum(tf.square(linear_model - y))  # sum of the squares
+cost = tf.reduce_sum(tf.square(hypothesis - y))  # sum of the squares
 
 # optimizer
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(loss)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
-# training data
-x_train = [1, 2, 3, 4]
-y_train = [0, -1, -2, -3]
+# training
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
 
-# training loop
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)  # reset values to wrong
-for i in range(1000):
-    sess.run(train, {x: x_train, y: y_train})
+    for step in range(1000):
+        sess.run(train, {x: x_train, y: y_train})
 
-# evaluate training accuracy
-curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
-print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
+    # evaluate training accuracy
+    W_val, b_val, cost_val = sess.run([W, b, cost], feed_dict={x: x_train, y: y_train})
+    print(f"W: {W_val} b: {b_val} cost: {cost_val}")
+
+"""
+W: [-0.9999969] b: [0.9999908] cost: 5.699973826267524e-11
+"""
